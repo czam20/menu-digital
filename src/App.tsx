7ac30841c310532894b13831b7a82629a18d584b
Branyer <branyervergarac@gmail.com>
2024-06-04 11:12:06 -0400

    adding routes initial config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-   <div></div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<>home</>} />
+        <Route path="/owner/register" element={<>registro propietario</>} />
+        <Route
+          path="/owner/waiter/register"
+          element={<>registro de meseros por propietario</>}
+        />
+        <Route
+          path="/owner/menu"
+          element={<>creacion y edicion de menu del propietario</>}
+        />
+        <Route path="/login" element={<>login</>} />
+        <Route path="/waiter" element={<>home mesero</>} />
+        <Route path="/menu/:menuid/:plateid" element={<>detalle menu</>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
